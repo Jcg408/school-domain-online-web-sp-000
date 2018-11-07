8f166d6134bea6e07fa3e8f34355db4eb1cbe6b5
Jessica Gladstone <jcg321@comcast.net>
2018-11-07 17:25:38 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
 
     def add_student (name, grade)        
         if @roster.keys.include?(grade)
-            @roster[grade]<<name
+            @roster[grade] << name
         else
             @roster[grade] = [name]      
        end
